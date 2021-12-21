621bb80e9f78348834a2c284621638aad731d2a6
Tinkermandota <erik.salomonson@gmail.com>
2021-12-21 16:28:33 +0100

    yesno

--- Botloop.py
+++ Botloop.py
@@ -1,8 +1,23 @@
 import random
 import numpy as np
 
+yes = ["yes", "yep", "fine", "okay", "okey", "ok", "affirmative", "yeah", "absolutely", "agreed", "all right", "alright", "assuredly", "aye", "beyond a doubt", "by all means", "certainly", "definitely", "gladly", "indubitably", "assuredly", "naturally", "of course", "ofcourse", "positively", "precisely", "sure", "surely", "undoubtedly", "unquestionably", "very well", "willingly"]
+no = ["no", "nay", "nix", "never", "not", "negative"]
+
+def participation_analysis(text):
+    yesno = [False, False]
+    for each in yes:
+        if each in text:
+            yesno[0] = True
+    for each in no:
+        if each in text:
+            yesno[1] = True
+    return yesno
+
 intro = ["Hello!","Good day to you!","Welcome friend!","Nice! A visitor!","I'm awake! At last!"]
 participation_request = ["Would you like to review a movie?","I am really good at predicting if your movie review is good or bad. Would you like to give it a try?","If you write a movie review, I will predict if it is good or bad. Sounds fun?","Please think of a movie you have seen. You will write a review for it, and then I will predict if that review is good or bad. Sounds good?","As you might know, I am a chatbot that can predict if your movie review is good or bad. Would you like to try it?"]
+participation_again = ["Would you like to review another movie?","Would you like to give it another try?","Would you like me to predict another review?","This is fun! Want to go again?","Would you like to try it again?"]
+participation_confused = ["Please be clearer."]
 affirmative = ["Cool!","Nice!","Let's go!","Here we go!","I'm ready!"]
 goodbye = ["Well, if you change your mind, I'm right...","Another time then!","OK cool, see you later then!","Alright, another time then.","OK, I understand. Another time, friend!"]
 title_request = ["What is the movie called? Please write the title.","What movie would you like to review? Please write the title.","What is the movie you want to review called?","What is the title of the movie you would like to review?","Please write the title of the movie you want to review."]
@@ -11,32 +26,34 @@ confused = ["Hmm, that's a tough one. Either you weren't too fond of it, or you 
 repeat_responses = ["Please give it another try!","You get another shot for the same movie, please try and be more specific!","Please write the review again. For my sake, please try to be more precise this time!","Please write the review again. I'm sure I will get it this time!","Let's give the same movie another shot. Please be slightly more on point time, so I can properly analyze your review!"]
 positive = ["Cool! That was a positive review, you seem to like <TITLE>","Nice, you seem to have enjoyed <TITLE> quite a bit. That's a good review.","A good review for a good movie, right? You seem to like that movie.","That's a positive review. <TITLE> must be a pretty good movie.","I'm glad you liked the movie <TITLE>. Maybe worth checking out?"]
 negative = ["Ouch, that sounds like a pretty bad movie to be honest. But, not all movie have to be good!","Sorry to hear that. I take that as a pretty bad review.","Oh, that definitely sounds like a pretty bad movie.","From your review, I would say that <TITLE> is a pretty bad movie...","OK, better stay away from <TITLE> then. That was a rather harsh review."]
-review_again = ["Would you like to review another movie?","Would you like to give it another try?","Would you like me to predict another review?","This is fun! Want to go again?","Would you like to try it again?"]
 
 print(random.choice(intro))
-yesorno = input(random.choice(participation_request) + "\n")
-# Implement some functionality that actually analyzes the response
-yesorno = True
-while yesorno:
-    print(random.choice(affirmative))
-    movietitle = input(random.choice(title_request) + "\n")
-    review = input(random.choice(review_request) + "\n")
-    repeat = True
-    while repeat:
-        repeat = False
-        # Implement some functionality to remove the movie title in the review
-        # Implement some functionality to input the review into the trained network
-        result = np.random.rand()
-        if result > 0.4 and result < 0.6:
-            repeat = True
-            print(random.choice(confused))
-            review = input(random.choice(repeat_responses) + "\n")
-        elif result > 0.6:
-            print(random.choice(positive))
-        else:
-            print(random.choice(negative))
-    
-    yesorno = input(random.choice(review_again) + "\n")
-    # Implement some functionality that actually analyzes the response
-    yesorno = True
+yesno = participation_analysis(input(random.choice(participation_request) + "\n"))
+prompt_review = True
+while prompt_review:
+    if yesno[0] == False and yesno[1] == False:
+        yesno = participation_analysis(input(random.choice(participation_confused) + "\n"))
+    elif yesno[0] and yesno[1]:
+        yesno = participation_analysis(input(random.choice(participation_confused) + "\n"))
+    elif yesno[0]:
+        print(random.choice(affirmative))
+        movietitle = input(random.choice(title_request) + "\n")
+        review = input(random.choice(review_request) + "\n")
+        repeat = True
+        while repeat:
+            repeat = False
+            # Implement some functionality to remove the movie title in the review
+            # Implement some functionality to input the review into the trained network
+            result = np.random.rand()
+            if result > 0.4 and result < 0.6:
+                repeat = True
+                print(random.choice(confused))
+                review = input(random.choice(repeat_responses) + "\n")
+            elif result > 0.6:
+                print(random.choice(positive))
+            else:
+                print(random.choice(negative))
+        yesno = participation_analysis(input(random.choice(participation_again) + "\n"))
+    else:
+        prompt_review = False
 print(random.choice(goodbye))
