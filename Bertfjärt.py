d1a4c414cb4ef48b7c36fb0a00e6d720125fc670
Gustav-Rixon <Gustav.rixon@gmail.com>
2021-12-21 17:43:58 +0100

    push

---
+++ Bertfjärt.py
@@ -0,0 +1,105 @@
+import time
+import torch.optim as optim
+import torch.nn as nn
+from transformers import BertTokenizer, BertModel
+from torchtext.legacy import datasets
+from torchtext.legacy import data
+from transformers import BertTokenizer
+import torch
+import random
+import numpy as np
+
+bert = BertModel.from_pretrained('bert-base-uncased')
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+
+class BERTGRUSentiment(nn.Module):
+    def __init__(self,
+                 bert,
+                 hidden_dim,
+                 output_dim,
+                 n_layers,
+                 bidirectional,
+                 dropout):
+
+        super().__init__()
+
+        self.bert = bert
+
+        embedding_dim = bert.config.to_dict()['hidden_size']
+
+        self.rnn = nn.GRU(embedding_dim,
+                          hidden_dim,
+                          num_layers=n_layers,
+                          bidirectional=bidirectional,
+                          batch_first=True,
+                          dropout=0 if n_layers < 2 else dropout)
+
+        self.out = nn.Linear(
+            hidden_dim * 2 if bidirectional else hidden_dim, output_dim)
+
+        self.dropout = nn.Dropout(dropout)
+
+    def forward(self, text):
+
+        # text = [batch size, sent len]
+
+        with torch.no_grad():
+            embedded = self.bert(text)[0]
+
+        # embedded = [batch size, sent len, emb dim]
+
+        _, hidden = self.rnn(embedded)
+
+        # hidden = [n layers * n directions, batch size, emb dim]
+
+        if self.rnn.bidirectional:
+            hidden = self.dropout(
+                torch.cat((hidden[-2, :, :], hidden[-1, :, :]), dim=1))
+        else:
+            hidden = self.dropout(hidden[-1, :, :])
+
+        # hidden = [batch size, hid dim]
+
+        output = self.out(hidden)
+
+        # output = [batch size, out dim]
+
+        return output
+
+
+HIDDEN_DIM = 256
+OUTPUT_DIM = 1
+N_LAYERS = 2
+BIDIRECTIONAL = True
+DROPOUT = 0.25
+
+model = BERTGRUSentiment(bert,
+                         HIDDEN_DIM,
+                         OUTPUT_DIM,
+                         N_LAYERS,
+                         BIDIRECTIONAL,
+                         DROPOUT)
+
+tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
+max_input_length = tokenizer.max_model_input_sizes['bert-base-uncased']
+init_token = tokenizer.cls_token
+eos_token = tokenizer.sep_token
+pad_token = tokenizer.pad_token
+unk_token = tokenizer.unk_token
+init_token_idx = tokenizer.convert_tokens_to_ids(init_token)
+eos_token_idx = tokenizer.convert_tokens_to_ids(eos_token)
+pad_token_idx = tokenizer.convert_tokens_to_ids(pad_token)
+unk_token_idx = tokenizer.convert_tokens_to_ids(unk_token)
+
+
+def predict_sentiment(model, tokenizer, sentence):
+    model.eval()
+    tokens = tokenizer.tokenize(sentence)
+    tokens = tokens[:max_input_length-2]
+    indexed = [init_token_idx] + \
+        tokenizer.convert_tokens_to_ids(tokens) + [eos_token_idx]
+    tensor = torch.LongTensor(indexed).to(device)
+    tensor = tensor.unsqueeze(0)
+    prediction = torch.sigmoid(model(tensor))
+    return prediction.item()
--- Botloop.py
+++ Botloop.py
@@ -1,8 +1,45 @@
 import random
+import torch
 import numpy as np
+from spellchecker import SpellChecker
+from Bertfjärt import predict_sentiment, model, tokenizer
+import re
+import time
+spell = SpellChecker()
+model.load_state_dict(torch.load('tut6-model.pt'))  # load in ze model
 
-yes = ["yes", "yep", "fine", "okay", "okey", "ok", "affirmative", "yeah", "absolutely", "agreed", "all right", "alright", "assuredly", "aye", "beyond a doubt", "by all means", "certainly", "definitely", "gladly", "indubitably", "assuredly", "naturally", "of course", "ofcourse", "positively", "precisely", "sure", "surely", "undoubtedly", "unquestionably", "very well", "willingly"]
+
+def spellCheck(text):
+    text = text.split(" ")
+    misspelled = spell.unknown(text)
+    for word in misspelled:
+        print(misspelled)
+        print(
+            f'Sorry are you retarded the word "{word}" is not a word. Did you mean "{spell.candidates(word)}"?')
+        time.sleep(1)
+        new_word = input("Please correct you mistake\n")
+        text[text.index(word)] = new_word  # WORD?
+    return text
+
+# TODO CHANGE FUNKTION NAME
+
+
+def removeTEMPNAME(text):
+    x = re.sub(r'".*"', '', text)
+    return x
+
+
+def listToString(s):
+    # initialize an empty string
+    str1 = " "
+    # return string
+    return (str1.join(s))
+
+
+yes = ["yes", "yep", "fine", "okay", "okey", "ok", "affirmative", "yeah", "absolutely", "agreed", "all right", "alright", "assuredly", "aye", "beyond a doubt", "by all means", "certainly",
+       "definitely", "gladly", "indubitably", "assuredly", "naturally", "of course", "ofcourse", "positively", "precisely", "sure", "surely", "undoubtedly", "unquestionably", "very well", "willingly"]
 no = ["no", "nay", "nix", "never", "not", "negative"]
+
 
 def participation_analysis(text):
     yesno = [False, False]
@@ -14,47 +51,75 @@ def participation_analysis(text):
             yesno[1] = True
     return yesno
 
-intro = ["Hello!","Good day to you!","Welcome friend!","Nice! A visitor!","I'm awake! At last!"]
-participation_request = ["Would you like to review a movie?","I am really good at predicting if your movie review is good or bad. Would you like to give it a try?","If you write a movie review, I will predict if it is good or bad. Sounds fun?","Please think of a movie you have seen. You will write a review for it, and then I will predict if that review is good or bad. Sounds good?","As you might know, I am a chatbot that can predict if your movie review is good or bad. Would you like to try it?"]
-participation_again = ["Would you like to review another movie?","Would you like to give it another try?","Would you like me to predict another review?","This is fun! Want to go again?","Would you like to try it again?"]
+
+intro = ["Hello!", "Good day to you!", "Welcome friend!",
+         "Nice! A visitor!", "I'm awake! At last!"]
+participation_request = ["Would you like to review a movie?", "I am really good at predicting if your movie review is good or bad. Would you like to give it a try?", "If you write a movie review, I will predict if it is good or bad. Sounds fun?",
+                         "Please think of a movie you have seen. You will write a review for it, and then I will predict if that review is good or bad. Sounds good?", "As you might know, I am a chatbot that can predict if your movie review is good or bad. Would you like to try it?"]
+participation_again = ["Would you like to review another movie?", "Would you like to give it another try?",
+                       "Would you like me to predict another review?", "This is fun! Want to go again?", "Would you like to try it again?"]
 participation_confused = ["Please be clearer."]
-affirmative = ["Cool!","Nice!","Let's go!","Here we go!","I'm ready!"]
-goodbye = ["Well, if you change your mind, I'm right here...","Another time then!","OK cool, see you later then!","Alright, another time then.","OK, I understand. Another time, friend!"]
-title_request = ["What is the movie called? Please write the title.","What movie would you like to review? Please write the title.","What is the movie you want to review called?","What is the title of the movie you would like to review?","Please write the title of the movie you want to review."]
-review_request = ["Write a review please!","Please type your review here!","Please write a review:","Now, please write a review:","Now, please write what you thought of the movie!"]
-review_again = ["Would you like to review another movie?","Would you like to give it another try?","Would you like me to predict another review?","This is fun! Want to go again?","Would you like to try it again?"]
-positive = ["Cool! That was a positive review, you seem to like <TITLE>","Nice, you seem to have enjoyed <TITLE> quite a bit. That's a good review.","A good review for a good movie, right? You seem to like that movie.","That's a positive review. <TITLE> must be a pretty good movie.","I'm glad you liked the movie <TITLE>. Maybe worth checking out?"]
-confused = ["Hmm, that's a tough one. Either you weren't too fond of it, or you didn't write a good enough review.","It is really hard to determine if it is a good or bad review when you are that vague.","You don't seem to know if you like it or not.","I can't really discern what you thought about the movie.","You have to be a bit more clear in your review. It is hard to know what you thought of the movie."]
-negative = ["Ouch, that sounds like a pretty bad movie to be honest. But, not all movies have to be good!","Sorry to hear that. I take that as a pretty bad review.","Oh, that definitely sounds like a pretty bad movie.","From your review, I would say that <TITLE> is a pretty bad movie...","OK, better stay away from <TITLE> then. That was a rather harsh review."]
-repeat_responses = ["Please give it another try!","You get another shot for the same movie, please try and be more specific!","Please write the review again. For my sake, please try to be more precise this time!","Please write the review again. I'm sure I will get it this time!","Let's give the same movie another shot. Please be slightly more on point time, so I can properly analyze your review!"]
+affirmative = ["Cool!", "Nice!", "Let's go!", "Here we go!", "I'm ready!"]
+goodbye = ["Well, if you change your mind, I'm right here...", "Another time then!",
+           "OK cool, see you later then!", "Alright, another time then.", "OK, I understand. Another time, friend!"]
+title_request = ["What is the movie called? Please write the title.", "What movie would you like to review? Please write the title.",
+                 "What is the movie you want to review called?", "What is the title of the movie you would like to review?", "Please write the title of the movie you want to review."]
+review_request = ["Write a review please!", "Please type your review here!", "Please write a review:",
+                  "Now, please write a review:", "Now, please write what you thought of the movie!"]
+review_again = ["Would you like to review another movie?", "Would you like to give it another try?",
+                "Would you like me to predict another review?", "This is fun! Want to go again?", "Would you like to try it again?"]
+positive = ["Cool! That was a positive review, you seem to like <TITLE>", "Nice, you seem to have enjoyed <TITLE> quite a bit. That's a good review.",
+            "A good review for a good movie, right? You seem to like that movie.", "That's a positive review. <TITLE> must be a pretty good movie.", "I'm glad you liked the movie <TITLE>. Maybe worth checking out?"]
+confused = ["Hmm, that's a tough one. Either you weren't too fond of it, or you didn't write a good enough review.", "It is really hard to determine if it is a good or bad review when you are that vague.",
+            "You don't seem to know if you like it or not.", "I can't really discern what you thought about the movie.", "You have to be a bit more clear in your review. It is hard to know what you thought of the movie."]
+negative = ["Ouch, that sounds like a pretty bad movie to be honest. But, not all movies have to be good!", "Sorry to hear that. I take that as a pretty bad review.",
+            "Oh, that definitely sounds like a pretty bad movie.", "From your review, I would say that <TITLE> is a pretty bad movie...", "OK, better stay away from <TITLE> then. That was a rather harsh review."]
+repeat_responses = ["Please give it another try!", "You get another shot for the same movie, please try and be more specific!", "Please write the review again. For my sake, please try to be more precise this time!",
+                    "Please write the review again. I'm sure I will get it this time!", "Let's give the same movie another shot. Please be slightly more on point time, so I can properly analyze your review!"]
 
 print(random.choice(intro))
-yesno = participation_analysis(input(random.choice(participation_request) + "\n"))
+yesno = participation_analysis(
+    input(random.choice(participation_request) + "\n"))
 prompt_review = True
+
 while prompt_review:
+
     if yesno[0] == False and yesno[1] == False:
-        yesno = participation_analysis(input(random.choice(participation_confused) + "\n"))
+        yesno = participation_analysis(
+            input(random.choice(participation_confused) + "\n"))
+
     elif yesno[0] and yesno[1]:
-        yesno = participation_analysis(input(random.choice(participation_confused) + "\n"))
+        yesno = participation_analysis(
+            input(random.choice(participation_confused) + "\n"))
+
     elif yesno[0]:
+
         print(random.choice(affirmative))
         movietitle = input(random.choice(title_request) + "\n")
         review = input(random.choice(review_request) + "\n")
         repeat = True
+
         while repeat:
+
             repeat = False
             # Implement some functionality to remove the movie title in the review
+            review = spellCheck(review)
             # Implement some functionality to input the review into the trained network
-            result = np.random.rand()
+            result = predict_sentiment(model, tokenizer, review)
+
             if result > 0.4 and result < 0.6:
                 repeat = True
                 print(random.choice(confused))
                 review = input(random.choice(repeat_responses) + "\n")
+
             elif result > 0.6:
                 print(random.choice(positive))
+
             else:
                 print(random.choice(negative))
-        yesno = participation_analysis(input(random.choice(participation_again) + "\n"))
+
+        yesno = participation_analysis(
+            input(random.choice(participation_again) + "\n"))
     else:
         prompt_review = False
 print(random.choice(goodbye))
